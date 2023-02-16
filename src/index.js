8860f803e72720125dd84ddeb5708299bf118446
Matt Glowacki <mattwglowacki@gmail.com>
2023-02-16 13:01:06 -0500

    Add event listeners to tabs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import contactPage from './contact.js'
 const init = (() => {
   placeTabs()
   homePage()
-
 })()
 
 function placeTabs() {
@@ -17,14 +16,17 @@ function placeTabs() {
   const homeTab = document.createElement('button')
   homeTab.classList.add('home-tab')
   homeTab.textContent = 'Home'
+  homeTab.addEventListener('click', () => renderHomeTab())
 
   const menuTab = document.createElement('button')
   menuTab.classList.add('menu-tab')
   menuTab.textContent = 'Menu'
+  menubar.addEventListener('click', () => renderMenuTab())
 
   const contactTab = document.createElement('button')
   contactTab.classList.add('contact-tab')
   contactTab.textContent = 'Contact'
+  contactTab.addEventListener = ('click', () => renderContactTab())
 
   tabContainer.appendChild(homeTab)
   tabContainer.appendChild(menuTab)
